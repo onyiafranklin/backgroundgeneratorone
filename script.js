2756ff6444e6582c73b39661a92e4ebcc61a1697
Ezugudor <nelsonsmrt@gmail.com>
2018-05-01 19:19:39 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 //Munwa bu Ezugudor addi dis thing
-var css = document.querySelector("h3");
+var css = document.querySelector("hf3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
