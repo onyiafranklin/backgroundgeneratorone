ac01bc4115eef644b5c5f912761d592a04f9240a
franklin ebuka onyia <37715429+onyiafranklin@users.noreply.github.com>
2018-05-01 20:32:16 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 
 //Munwa44 bu Ezugudor addi dis thing
 var css = document.querySelector("hfgdgd3");
->>>>>>> 00f11ebc00b37040bffaf00da870b7d3b716d67f
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
